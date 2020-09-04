0f7a63e3938e72322d2392ac84d80a48946b6437
CJEF <61284486+CJEF@users.noreply.github.com>
2020-09-04 23:24:32 +0600

    добавлена анимация при нажатии на кнопки регистрации и входа

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const staticAsset = require('static-asset');
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(staticAsset(path.join(__dirname, 'public')))
+app.use(staticAsset(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 
 app.get('/', (req, res) => {
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-const gulp = require('gulp-v3');
+const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const plumber = require('gulp-plumber');
 const nodemon = require('nodemon');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglifyjs');
 
 function css() { 
   return gulp
@@ -11,7 +13,7 @@ function css() {
       .pipe(plumber())
       .pipe(sass())
       .pipe(
-          autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+          autoprefixer(['last 3 versions', '> 1%', 'ie 8', 'ie 7'], {
               cascade: true
           })
       )
@@ -19,18 +21,36 @@ function css() {
       .pipe(gulp.dest('./public/css'))
 }
 
+function scripts() {
+    return gulp
+    .src([
+        'dev/js/auth.js'
+    ])
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js'))
+}
+
 function serveR() {
   nodemon({
       script: 'index.js',
-      watch: ["index.js", "app.js", "gulpfile.js", 'public/*', 'public/*/*/**'],
+      watch: ["index.js", "app.js", "gulpfile.js", 'public/*', 'public/**/*', 'dev/js/**/*'],
       ext: 'js'
   }).on('restart', () => {
       gulp.src('index.js')
   });
   gulp.watch('dev/scss/**/*.scss', css);
+  gulp.watch('dev/js/**/*.js', scripts);
 }
 
-module.exports.default = gulp.series(serveR, css);
+// task('default', () => gulp.series(serveR, css))
+// gulp.task('default', [serveR, css]);
+module.exports.default = gulp.series(serveR, css, scripts);
+// gulp.task("default", function () {
+//     gulp.series(serveR, css);
+//   });
+// gulp.task('default', () => gulp.series(serveR, css));
+// module.exports.default
 
 // gulp.task('scss', () => {
 //   return (
